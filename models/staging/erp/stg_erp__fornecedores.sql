8fbe3e9c489e870c85a9075a17145fc221c4e7d7
Vitor Lira <vitorcavalcantilira@gmail.com>
2024-09-28 18:55:26 +0000

    Ajuste em Intermediate Produtos Enriquecida

--- models/staging/erp/stg_erp__fornecedores.sql
+++ models/staging/erp/stg_erp__fornecedores.sql
@@ -1,21 +1,12 @@
 with
-    renomeado as (
+    fonte_fornecedores as (
         select
-            cast(ID as int) as pk_produto
-            , cast(SUPPLIERID as int) as fk_fornecedor 
-            , cast(CATEGORYID as int) as fk_categoria
-            , cast(PRODUCTNAME as string) as nm_produto
-            , cast(QUANTITYPERUNIT as string) as quantidade_por_unidade
-            , cast(UNITPRICE as numeric(18,2)) as preco_por_unidade
-            , cast(UNITSINSTOCK as int) as unidade_em_estoque
-            , cast(UNITSONORDER as int) as unidade_por_pedido
-            , cast(REORDERLEVEL as int) as nivel_de_pedido
-            , case
-                when discontinued = '1' then true
-                when discontinued = '0' then false
-                else null
-            end as eh_discontinuado
-        from {{ source('erp_northwind', 'product') }}
+            cast(ID as int) as pk_fornecedor
+            , cast(COMPANYNAME as varchar) as nome_fornecedor
+            , cast(CITY as varchar) as cidade_fornecedor
+            , cast(COUNTRY as varchar) as pais_fornecedor
+        from {{ source('erp_northwind', 'supplier') }}
     )
+
 select *
-from renomeado
+from fonte_fornecedores
